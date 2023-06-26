4cdd7a39f76412bb9885bfd3e77f6cf663931b5e
nadine525 <02_seeders_wide@icloud.com>
2023-06-26 18:50:56 +0200

    st

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -11,20 +11,4 @@ export const HeaderContainer = styled.header`
   box-shadow: 0px 4px 36px -21px rgba(0, 0, 0, 0.75);
 `;
 
-export const StyledLink = styled(NavLink)`
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 20px;
-  letter-spacing: 0.03em;
-  cursor: pointer;
-  text-decoration: none;
-  color: #363434;
 
-  &:hover {
-    text-decoration: underline;
-  }
-
-  &.active {
-    color: #c75353;
-  }
-`;
