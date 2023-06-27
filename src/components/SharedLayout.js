9572179ad2234024e335201a87a4b9063089b1a8
nadine525 <02_seeders_wide@icloud.com>
2023-06-27 14:19:46 +0200

    Update SharedLayout.js

--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,19 +1,10 @@
-// import { NavLink } from 'react-router-dom';
 import Header from './Header/Header';
 import { Outlet } from 'react-router-dom';
-
-// import { Header, StyledLink } from './SharedLayout.styled';
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
-      {/* <Header>
-        <nav>
-          <StyledLink to="/">Home</StyledLink>
-          <StyledLink to="/movies">Movies</StyledLink>
-        </nav>
-      </Header> */}
 
       <main>
         <Outlet />
