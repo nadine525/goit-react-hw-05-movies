e1c5c152a08361fd99cfaca9b97dfc864b84ab2b
nadine525 <02_seeders_wide@icloud.com>
2023-06-27 15:41:08 +0200

    rebuilt

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import { getTrendingMovies } from 'servises/requestApi';
 
 const Home = () => {
   return <div>Домашня сторінка</div>;
-  getTrendingMovies().then(data => console.log(data));
 };
 
 export default Home;
