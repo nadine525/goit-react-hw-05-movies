9f0c3f1d12543fcaf3130ad46b65edcedb5fe28c
nadine525 <02_seeders_wide@icloud.com>
2023-06-28 23:07:23 +0200

    more two request api

--- src/servises/requestApi.js
+++ src/servises/requestApi.js
@@ -19,3 +19,31 @@ export const getMovieDetails = async id => {
 
   return response.data;
 };
+// getTrendingMovies().then(data => console.log(data)); //check
+
+export const getMoviesFactorsCast = async id => {
+  const response = await axios.get(
+    `${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
+  );
+
+  return response.cast;
+};
+// getMovieDetails(455476).then(data => console.log(data)); // check
+
+export const getMovieActorsCast = async id => {
+  const response = await axios.get(
+    `${BASE_URL}/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
+  );
+
+  return response.data.cast;
+};
+// getMoviesActorsCast(455476).then(cast => console.log(cast)); // check
+
+export const getMovieReviews = async id => {
+  const response = await axios.get(
+    `${BASE_URL}/movie/${id}/reviews?api_key=${API_KEY}&language=en-US`
+  );
+
+  return response.data.results;
+};
+// getMovieReviews(385687).then(results => console.log(results));
