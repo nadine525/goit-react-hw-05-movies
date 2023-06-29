66e74f052eae7f06da8210c2ce1871280ed3e60d
nadine525 <02_seeders_wide@icloud.com>
2023-06-29 13:06:04 +0200

    cast

--- src/pages/Cast.js
+++ src/pages/Cast.js
@@ -1,5 +1,56 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieActorsCast } from '../servises/requestApi';
+import { List, ActorName, Character } from './Cast.styled';
+
 const Cast = () => {
-  return <div>Фото акторів</div>;
+  const [cast, setCast] = useState(null);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    getMovieActorsCast(movieId)
+      .then(data => setCast(data))
+      .catch(error => console.log(error));
+  }, [movieId]);
+
+  console.log(cast);
+
+  if (!cast) {
+    return;
+  }
+
+  return (
+    <>
+      {cast.length > 0 ? (
+        <List>
+          {cast.map(({ id, name, character, profile_path }) => (
+            <li key={id}>
+              {profile_path ? (
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${profile_path}`}
+                  alt={name}
+                  width="150"
+                />
+              ) : (
+                <img
+                  src={
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1oJta_1HqNcTUlR5HGGzNOvJRUy0RDRavjw&usqp=CAU'
+                  }
+                  alt="not found"
+                  width="150"
+                  height="150"
+                />
+              )}
+              <ActorName>{name}</ActorName>
+              <Character>Character: {character}</Character>
+            </li>
+          ))}
+        </List>
+      ) : (
+        <div>No casts</div>
+      )}
+    </>
+  );
 };
 
 export default Cast;
---
+++ src/pages/Cast.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  padding-top: 15px;
+  padding-bottom: 15px;
+`;
+
+export const ActorName = styled.p`
+  margin-bottom: 8px;
+  font-size: 16px;
+`;
+
+export const Character = styled.p`
+  margin-bottom: 12px;
+  font-size: 14px;
+`;
